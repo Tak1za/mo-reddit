c54e35a20048e98702e35bc8b71072d655e3ffc5
Varun <varungupta2015135@gmail.com>
2021-08-26 02:02:45 +0530

    Added graphql endpoints to register and login user, CRUD on posts

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import redis from "redis";
+
+export = session;
+
+declare module "express-session" {
+  interface SessionData {
+    userId: any;
+  }
+}
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+      },
+      secret: "nbfvdjkfgnrkjlngkdrngsmohopzdshjp",
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({
+      em: orm.em,
+      req,
+      res,
+    }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on port 4000");
+  });
+};
+
+main().catch((err) => console.error(err));
